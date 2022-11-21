1029c7db656248516998bd73842badff85292bae
Sun_r <rithsenghorn@gmail.com>
2022-11-21 10:39:48 -0700

    login screen impl, takes user to home upon success login

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,13 +1,14 @@
-import { NavigationContainer } from '@react-navigation/native';
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
-import BottomNavigation from './components/bottomNavigation';
+import 'react-native-gesture-handler';
+import Navigation from './components/Navigation'
+import { NavigationContainer} from "@react-navigation/native";
 
 export default function App() {
-
   return (
     <NavigationContainer>
-      <BottomNavigation />
+      <Navigation/>
       <StatusBar style="auto" />
     </NavigationContainer>
   );
--- Frontend/components/bottomNavigation.js
+++ Frontend/components/bottomNavigation.js
@@ -1,11 +1,17 @@
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import Notes from '../screens/notes';
-import Messages from '../screens/messages';
-import Metrics from '../screens/metrics';
-import Calendar from '../screens/calendar';
-import Tasks from '../screens/tasks';
-import LogIn from '../screens/login'
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import Notes from "../screens/notes";
+import Messages from "../screens/messages";
+import Metrics from "../screens/metrics";
+import Calendar from "../screens/calendar";
+import Tasks from "../screens/tasks";
+import LogIn from "../screens/login";
+
+// const Drawer = createDrawerNavigator();
+// <Drawer.Navigator initialRouteName="Home">
+//   <Drawer.Screen name="Home" component={HomeScreen} />
+//   <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+// </Drawer.Navigator>
 
 const Tab = createMaterialBottomTabNavigator();
 
@@ -15,35 +21,58 @@ export default function BottomNavigation() {
       <Tab.Screen
         name="Notes"
         component={Notes}
-        options={{ tabBarIcon: ({ color }) => <MaterialCommunityIcons name="notebook" color={color} size={26} /> }}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="notebook" color={color} size={26} />
+          ),
+        }}
       />
       <Tab.Screen
         name="Messages"
         component={Messages}
-        options={{ tabBarIcon: ({ color }) => <MaterialCommunityIcons name="android-messages" color={color} size={26} /> }}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="android-messages"
+              color={color}
+              size={26}
+            />
+          ),
+        }}
       />
       <Tab.Screen
         name="Calendar"
         component={Calendar}
-        options={{ tabBarIcon: ({ color }) => <MaterialCommunityIcons name="calendar-heart" color={color} size={26} /> }}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="calendar-heart"
+              color={color}
+              size={26}
+            />
+          ),
+        }}
       />
       <Tab.Screen
         name="Metrics"
         component={Metrics}
-        options={{ tabBarIcon: ({ color }) => <MaterialCommunityIcons name="chart-box" color={color} size={26} /> }}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="chart-box" color={color} size={26} />
+          ),
+        }}
       />
       <Tab.Screen
         name="Tasks"
         component={Tasks}
         options={{
-          tabBarIcon: ({ color }) => <MaterialCommunityIcons name="checkbox-multiple-marked" color={color} size={26} />,
-        }}
-      />
-      <Tab.Screen
-        name="Login"
-        component={LogIn}
-        options={{
-          tabBarIcon: ({ color }) => <MaterialCommunityIcons name="account" color={color} size={26} />,
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="checkbox-multiple-marked"
+              color={color}
+              size={26}
+            />
+          ),
         }}
       />
     </Tab.Navigator>
