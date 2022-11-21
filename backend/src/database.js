61873e7167a672f79ee5abad7157a2100988aeb9
Aaron Heo <aheo@qualtrics.com>
2022-11-20 23:38:46 -0700

    GET /notes/:groupId

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,21 +1,15 @@
 // get the client
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
+module.exports.query = async (query) => {
 // create the connection to database
-const connection = mysql.createConnection({
-  host: 'caretechers-db.cyvcqcx1nbv9.us-east-1.rds.amazonaws.com',
-  user: 'admin',
-  database: 'cctest',
-  password: ''
-});
+	const connection = await mysql.createConnection({
+		host: 'caretechers-db.cyvcqcx1nbv9.us-east-1.rds.amazonaws.com',
+		user: 'admin',
+		database: 'carecoord',
+		password: '4$CareTechers'
+	});
 
-// execute will internally call prepare and query
-connection.execute(
-  'SELECT * FROM `User`',
-  [],
-  function(err, results, fields) {
-    console.log(results); // results contains rows returned by server
-    console.log(fields); // fields contains extra meta data about results, if available
-
-  }
-);
+	const [result]= await connection.execute(query.sql, query.values);
+	return result;
+}
