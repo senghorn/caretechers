892afd899d8d9a251e5d589514e6702a7e9b3d51
Aaron Heo <aheo@qualtrics.com>
2022-11-21 13:51:19 -0700

    Create POST notes/group/:groupId

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,8 @@ const notes = require('./routes/notes');
 const app = express()
 const port = 3000
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
 	res.status(200).send('Healthcheck successful!')
 })
--- backend/src/utls.js
+++ backend/src/utls.js
@@ -3,3 +3,13 @@ module.exports.newError = (message, status) => {
 	error.status = status;
 	return error;
 }
+
+module.exports.getUTCDateTime = () => {
+	const date = new Date()
+	return date.getUTCFullYear() + '-' +
+			('00' + (date.getUTCMonth()+1)).slice(-2) + '-' +
+			('00' + date.getUTCDate()).slice(-2) + ' ' +
+			('00' + date.getUTCHours()).slice(-2) + ':' +
+			('00' + date.getUTCMinutes()).slice(-2) + ':' +
+			('00' + date.getUTCSeconds()).slice(-2);
+}
