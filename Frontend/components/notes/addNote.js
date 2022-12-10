3b446ccc1a3b1f2630a3a363475817a7c64e7579
Sun_r <rithsenghorn@gmail.com>
2022-12-10 14:29:25 -0700

    working on fetching note from backend

---
+++ Frontend/components/notes/addNote.js
@@ -0,0 +1,128 @@
+import { useState, useRef } from "react";
+import {
+  View,
+  StyleSheet,
+  Modal,
+  TextInput,
+  Text,
+} from "react-native";
+import { Button } from "react-native-paper";
+import COLORS from "../../constants/colors";
+
+export default function AddNote({
+  notes,
+  setNotes,
+  modalVisible,
+  setModalVisible,
+}) {
+  const [noteTitle, setNoteTitle] = useState(""); // Add a state variable for the note title
+  const [noteContent, setNoteContent] = useState(""); // Add a state variable for the note content
+  const [newId, setNewId] = useState(10); // TODO: switch up this new ID generator
+
+  // Function to add a new note
+  const addNote = () => {
+    setNotes([
+      ...notes,
+      {
+        title: noteTitle,
+        content: noteContent,
+        id: newId,
+      },
+    ]);
+    setNewId(newId + 1); // Generate new id
+    setNoteTitle(""); // Clear the note title field
+    setNoteContent(""); // Clear the note content field
+    setModalVisible(false); // Hide the modal
+  };
+
+  const cancelNote = () => {
+    setNoteTitle(""); // Clear the note title field
+    setNoteContent(""); // Clear the note content field
+    setModalVisible(false); // Hide the modal
+  };
+
+  return (
+    <Modal visible={modalVisible}>
+      <View style={styles.modal}>
+        <Text style={styles.label}>Note title</Text>
+        <TextInput
+          style={styles.input}
+          value={noteTitle}
+          onChangeText={(text) => setNoteTitle(text)}
+          labelStyle={styles.noteInput}
+        />
+        <Text style={styles.label}>Note description</Text>
+        <TextInput
+          style={styles.input}
+          value={noteContent}
+          onChangeText={(text) => setNoteContent(text)}
+          labelStyle={styles.noteInput}
+        />
+        <View style={styles.row}>
+          <Button
+            mode="contained"
+            uppercase={false}
+            color={COLORS.danger}
+            icon="cancel"
+            onPress={() => cancelNote()}
+            style={styles.cancelNoteButton}
+            labelStyle={styles.createButtonText}
+          >
+            Cancel
+          </Button>
+          <Button
+            mode="contained"
+            uppercase={false}
+            color="#2196f3"
+            icon="checkbox-marked-circle-plus-outline"
+            onPress={() => addNote()}
+            style={styles.addNoteButton}
+            labelStyle={styles.createButtonText}
+          >
+            Add Note
+          </Button>
+        </View>
+      </View>
+    </Modal>
+  );
+}
+const styles = StyleSheet.create({
+  modal: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ddd",
+    padding: 10,
+    marginBottom: 10,
+    width: "50%",
+    color: "#333",
+  },
+  cancelNoteButton: {
+    marginVertical: 32,
+    width: "40%",
+    alignSelf: "center",
+    marginEnd: 10,
+  },
+  addNoteButton: {
+    marginVertical: 32,
+    width: "40%",
+    alignSelf: "center",
+    marginStart: 10,
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  row: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    alignSelf: "center",
+    alignItems: "center",
+  },
+  noteInput: {
+    fontSize: 14,
+  },
+});
--- Frontend/screens/notes.js
+++ Frontend/screens/notes.js
@@ -1,128 +1,47 @@
-import { useState } from "react";
-import {
-  View,
-  StyleSheet,
-  ScrollView,
-  Modal,
-  TextInput,
-  Text,
-} from "react-native";
+import { useState, useEffect } from "react";
+import { View, StyleSheet, ScrollView } from "react-native";
 import { Button } from "react-native-paper";
 import Note from "../components/notes/note";
 import COLORS from "../constants/colors";
 import Header from "../components/notes/header";
+import AddNote from "../components/notes/addNote";
+
+const fetchNotes = async (setNotes) => {
+  const result = await fetch("http://192.168.0.43:3000/notes/group/1");
+  const data = await result.text();
+  console.log(data);
+  // setNotes(data);
+  console.log("fetching notes");
+};
 
 export default function Notes() {
   const [notes, setNotes] = useState([
-    {
-      title: "Wifi",
-      content: "SSID: Caretechers passcode: 123123",
-      id: 1,
-    },
-    {
-      title: "Garage",
-      content: "code: 312312",
-      id: 2,
-    },
-    {
-      title: "Door",
-      content: "code: 123321",
-      id: 3,
-    },
-    {
-      id: 4,
-      title: "Water Heater",
-      content:
-        "How to fix water heater: CLOSE gas pipe, slide the heater small glass window, turn on pilot and then use lighter to ignite the pilot. Hold on to pilot for 1mn.",
-    },
-    {
-      id: 5,
-      title: "AC",
-      content:
-        "How to use AC: located in the second floor master bedroom. Adjust it accordingly.",
-    },
+    { title: "Hi", content: "Hello", id: 100 },
   ]);
+  useEffect(() => {
+    fetchNotes(setNotes);
+  }, []);
 
-  const [modalVisible, setModalVisible] = useState(false); // Add a state variable to control the visibility of the modal
-  const [noteTitle, setNoteTitle] = useState(""); // Add a state variable for the note title
-  const [noteContent, setNoteContent] = useState(""); // Add a state variable for the note content
-  const [newId, setNewId] = useState(10);
+  // Add a state variable to control the visibility of the modal
+  const [modalVisible, setModalVisible] = useState(false);
 
-  // Function to add a new note
-  const addNote = () => {
-    setNotes([
-      ...notes,
-      {
-        title: noteTitle,
-        content: noteContent,
-        id: newId,
-      },
-    ]);
-    setNewId(newId + 1); // Generate new id
-    setNoteTitle(""); // Clear the note title field
-    setNoteContent(""); // Clear the note content field
-    setModalVisible(false); // Hide the modal
-  };
-
-  const cancelNote = () => {
-    setNoteTitle(""); // Clear the note title field
-    setNoteContent(""); // Clear the note content field
-    setModalVisible(false); // Hide the modal
-  };
   return (
     <View style={styles.container}>
       <Header />
-
-      {/* Add new note view */}
-      <Modal visible={modalVisible}>
-        <View style={styles.modal}>
-          <Text style={styles.label}>Note title</Text>
-          <TextInput
-            style={styles.input}
-            placeholder="Note Title"
-            value={noteTitle}
-            onChangeText={(text) => setNoteTitle(text)}
-            labelStyle={styles.noteInput}
-          />
-          <Text style={styles.label}>Note description</Text>
-          <TextInput
-            style={styles.input}
-            placeholder="Note Content"
-            value={noteContent}
-            onChangeText={(text) => setNoteContent(text)}
-            labelStyle={styles.noteInput}
-          />
-          <View style={styles.row}>
-            <Button
-              mode="contained"
-              uppercase={false}
-              color={COLORS.danger}
-              icon="cancel"
-              onPress={() => cancelNote()}
-              style={styles.cancelNoteButton}
-              labelStyle={styles.createButtonText}
-            >
-              Cancel
-            </Button>
-            <Button
-              mode="contained"
-              uppercase={false}
-              color="#2196f3"
-              icon="checkbox-marked-circle-plus-outline"
-              onPress={() => addNote()}
-              style={styles.addNoteButton}
-              labelStyle={styles.createButtonText}
-            >
-              Add Note
-            </Button>
-          </View>
-        </View>
-      </Modal>
-
+      <AddNote
+        notes={notes}
+        setNotes={setNotes}
+        modalVisible={modalVisible}
+        setModalVisible={setModalVisible}
+      />
       <ScrollView style={styles.tasksContainer}>
         {/* Map over the notes and create a Note component for each note */}
         {notes.map((note) => (
-          <Note key={note.id} title={note.title} content={note.content} />
+          <Note
+            key={note["id"]}
+            title={note["title"]}
+            content={note["content"]}
+          />
         ))}
       </ScrollView>
       <Button
@@ -130,9 +49,9 @@ export default function Notes() {
         uppercase={false}
         color="#2196f3"
         icon="checkbox-marked-circle-plus-outline"
-        onPress={() => setModalVisible(true)}
         style={styles.createButton}
         labelStyle={styles.createButtonText}
+        onPress={() => setModalVisible(true)}
       >
         Add New Note
       </Button>
@@ -145,12 +64,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     paddingTop: 40,
-  },
-  row: {
-    flexDirection: "row",
-    flexWrap: "wrap",
-    alignSelf: "center",
-    alignItems: "center",
   },
   headerContainer: {
     flexDirection: "row",
@@ -174,37 +87,5 @@ const styles = StyleSheet.create({
   },
   createButtonText: {
     fontSize: 14,
-  },
-  noteInput: {
-    fontSize: 14,
-  },
-  modal: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: '#ddd',
-    padding: 10,
-    marginBottom: 10,
-    width: "80%",
-    color: '#333',
-  },
-  cancelNoteButton: {
-    marginVertical: 32,
-    width: "40%",
-    alignSelf: "center",
-    marginEnd: 10,
-  },
-  addNoteButton: {
-    marginVertical: 32,
-    width: "40%",
-    alignSelf: "center",
-    marginStart: 10,
-  },
-  label: {
-    fontSize: 18,
-    marginBottom: 10,
   },
 });
