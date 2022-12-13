145d6f495e38ea18e09ab22c0492ed7ec4f1cc9f
benjhatch <benjamin.hat4@gmail.com>
2022-12-12 23:47:49 -0700

    updated design document and backend config

--- Frontend/constants/config.js
+++ Frontend/constants/config.js
@@ -1,3 +1,3 @@
 export default {
-    backend_server: '192.168.0.43:3000', // Update config when testing.
-}
+  backend_server: 'ec2-54-153-120-183.us-west-1.compute.amazonaws.com:3000', // Update config when testing.
+};
--- Frontend/screens/notes.js
+++ Frontend/screens/notes.js
@@ -1,16 +1,15 @@
-import { useState, useEffect } from "react";
-import { View, StyleSheet, ScrollView } from "react-native";
-import { Button } from "react-native-paper";
-import Note from "../components/notes/note";
-import COLORS from "../constants/colors";
-import Header from "../components/notes/header";
-import CreateNoteModal from "../components/notes/createNoteModal";
-import config from "../constants/config";
+import { useState, useEffect } from 'react';
+import { View, StyleSheet, ScrollView } from 'react-native';
+import { Button } from 'react-native-paper';
+import Note from '../components/notes/note';
+import COLORS from '../constants/colors';
+import Header from '../components/notes/header';
+import CreateNoteModal from '../components/notes/CreateNoteModal';
+import config from '../constants/config';
 
 const fetchNotes = async (setNotes) => {
   try {
-    let connection_string =
-      "http://" + config.backend_server + "/notes/group/1";
+    let connection_string = 'http://' + config.backend_server + '/notes/group/1';
     const result = await fetch(connection_string);
     const data = await result.json();
     setNotes(data);
@@ -25,19 +24,13 @@ export default function Notes() {
     fetchNotes(setNotes);
   }, []);
 
-
   // Add a state variable to control the visibility of the modal
   const [modalVisible, setModalVisible] = useState(false);
 
   return (
     <View style={styles.container}>
       <Header />
-      <CreateNoteModal
-        notes={notes}
-        setNotes={setNotes}
-        modalVisible={modalVisible}
-        setModalVisible={setModalVisible}
-      />
+      <CreateNoteModal notes={notes} setNotes={setNotes} modalVisible={modalVisible} setModalVisible={setModalVisible} />
       <ScrollView style={styles.tasksContainer}>
         {/* Map over the notes and create a Note component for each note */}
         {notes.map((note) => (
@@ -62,12 +55,12 @@ export default function Notes() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: '#fff',
     paddingTop: 40,
   },
   headerContainer: {
-    flexDirection: "row",
-    flexBasis: "auto",
+    flexDirection: 'row',
+    flexBasis: 'auto',
   },
   box: {
     elevation: 0,
@@ -76,14 +69,14 @@ const styles = StyleSheet.create({
   },
   tasksContainer: {
     flex: 1,
-    width: "100%",
+    width: '100%',
     paddingHorizontal: 24,
     marginTop: 16,
   },
   createButton: {
     marginVertical: 32,
-    width: "40%",
-    alignSelf: "center",
+    width: '40%',
+    alignSelf: 'center',
   },
   createButtonText: {
     fontSize: 14,
