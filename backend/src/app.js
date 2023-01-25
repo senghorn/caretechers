b620e74191ceba5150262806f4bfbb2acd8aef94
benjhatch <benjamin.hat4@gmail.com>
2023-01-24 17:23:40 -0700

    added endpoints to fetch tasks over a date range as well as visit history over a date range

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,29 +1,30 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
-	res.status(200).send('Healthcheck successful!')
-})
+  res.status(200).send('Healthcheck successful!');
+});
 
 // MAIN API ENDPOINTS
 app.use('/notes', require('./routes/notes'));
 app.use('/messages', require('./routes/messages'));
 app.use('/tasks', require('./routes/tasks'));
+app.use('/visits', require('./routes/visits'));
 // ------------------
 
 app.use((err, req, res, next) => {
-	console.error({
-		status: err.status || '',
-		message: err.message || '',
-		stack: err.stack || ''
-	});
-	res.status(err.status || 500).send(err.message || '');
-})
+  console.error({
+    status: err.status || '',
+    message: err.message || '',
+    stack: err.stack || '',
+  });
+  res.status(err.status || 500).send(err.message || '');
+});
 
 app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
