99612f7e1f1aef583621a58318c7e9e5ee1c1b92
Brynnli <bucketbrynn@gmail.com>
2023-02-25 16:04:29 -0700

    another commit

--- backend/src/rest-server.js
+++ backend/src/rest-server.js
@@ -31,25 +31,11 @@ function CreateRESTServer() {
     definition: {
       openapi: "3.0.0",
       info: {
-        title: "LogRocket Express API with Swagger",
-        version: "0.1.0",
+        title: "Caretechers CareCoord API",
+        version: "1.0.0",
         description:
-          "This is a simple CRUD API application made with Express and documented with Swagger",
-        license: {
-          name: "MIT",
-          url: "https://spdx.org/licenses/MIT.html",
-        },
-        contact: {
-          name: "LogRocket",
-          url: "https://logrocket.com",
-          email: "info@email.com",
-        },
-      },
-      servers: [
-        {
-          url: "http://localhost:3000",
-        },
-      ],
+        "The API used by the CareCoord App"
+      }
     },
     apis: ["./src/routes/*.js"],
   };
--- backend/src/routes/groups.js
+++ backend/src/routes/groups.js
@@ -11,9 +11,60 @@ router.post('/', [
 	sharedMiddleware.sendResult
 ]);
 
+  /**
+ * @swagger
+ * tags:
+ *   name: Groups
+ * /groups:
+ *   post:
+ *     summary: Create a new group
+ *     tags: [Groups]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Group'
+ *     responses:
+ *       204:
+ *         description: The POST was successful, no content returned
+ *       400:
+ *         description: Body is formatted incorrectly
+ *
+ */
+
 router.get('/:limit',[
 	groupMiddleware.getGroups,
 	sharedMiddleware.sendResult
 ]);
 // testing fdajfljdalfdjal;fljs
 module.exports = router;
+
+  /**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Group:
+ *       type: object
+ *       required:
+ *         - id
+ *         - name
+ *         - visit_frequency
+ *         - password
+ *       properties:
+ *         id:
+ *           type: integer
+ *           description: The auto-generated id of the group
+ *         name:
+ *           type: string
+ *           description: The name of the group
+ *         visit_frequency:
+ *           type: integer
+ *           description: The frequency that the group wants to visit their elderly loved one
+ *         timezone:
+ *           type: string
+ *           description: The timezone at which the elderly loved one lives
+ *         password:
+ *           type: string
+ *           description: The password used to join the group
+ */
