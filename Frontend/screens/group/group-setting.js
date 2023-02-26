52bbc36ae8a0d6cec0e48f47050f864a684f201b
senghorn <rithsenghorn@gmail.com>
2023-02-26 10:44:17 -0700

    group init

--- Frontend/screens/group/group-setting.js
+++ Frontend/screens/group/group-setting.js
@@ -1,7 +1,11 @@
-import { SafeAreaView, View, StyleSheet, Text, TextInput } from 'react-native';
-import { Appbar } from 'react-native-paper';
+import { View, StyleSheet, Text } from 'react-native';
+import { Appbar, IconButton } from 'react-native-paper';
+import { useState, useEffect } from 'react';
+import colors from '../../constants/colors';
 
 export default function GroupSettings({ navigation }) {
+  const [bellOn, setBellOn] = useState(false);
+
   return (
     <View style={styles.container}>
       <Appbar.Header style={styles.headerContainer}>
@@ -11,8 +15,38 @@ export default function GroupSettings({ navigation }) {
             navigation.goBack();
           }}
         />
-        <Appbar.Content title={'Group'} titleStyle={styles.title} />
+        <Appbar.Content title={''} titleStyle={styles.title} />
       </Appbar.Header>
+      <View style={styles.bodyContainer}>
+        <View style={styles.titleBox}>
+          <Text style={styles.titleText}>Madison Family</Text>
+        </View>
+        <View style={styles.buttonList}>
+          <View style={styles.buttonContainer}>
+            <IconButton
+              icon='account-plus-outline'
+              size={20}
+              color={colors.white}
+              style={styles.iconButton}
+              onPress={() => console.log('Add User Pressed')}
+            />
+            <Text style={styles.buttonTitle}>Add</Text>
+          </View>
+          <View style={styles.buttonContainer}>
+            <IconButton
+              icon={bellOn ? 'bell-outline' : 'bell-off-outline'}
+              size={20}
+              color={colors.white}
+              style={styles.iconButton}
+              onPress={() => setBellOn(!bellOn)}
+            />
+            <Text style={styles.buttonTitle}>{bellOn ? 'Mute' : 'Unmute'}</Text>
+          </View>
+        </View>
+        <View style={styles.memberListBox}>
+          <Text>Members</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -22,14 +56,51 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
-  input: {
-    backgroundColor: '#900',
-  },
   headerContainer: {
     flex: 0,
     backgroundColor: '#fff',
   },
-  title: {
+  bodyContainer: {
+    flex: 1,
+  },
+  titleBox: {
+    backgroundColor: colors.profileCard,
+    justifyContent: 'center',
+    alignContent: 'center',
+    alignItems: 'center',
+    height: 120,
+    borderBottomLeftRadius: 70,
+    borderBottomEndRadius: 70,
+    marginLeft: 20,
+    marginRight: 20,
+  },
+  titleText: {
     fontSize: 18,
+    fontWeight: '500',
+    color: colors.gray,
+    alignSelf: 'center',
+  },
+  memberListBox: {
+    margin: 20,
+  },
+  iconButton: {
+    backgroundColor: colors.black,
+  },
+  buttonTitle: {
+    alignSelf: 'center',
+    marginTop: -6,
+    fontSize: 12,
+    color: colors.gray,
+  },
+  buttonContainer: {
+    alignSelf: 'center',
+    alignContent: 'center',
+    alignItems: 'center',
+    minWidth: 60,
+  },
+  buttonList: {
+    flexDirection: 'row',
+    alignContent: 'center',
+    justifyContent: 'center',
   },
 });
