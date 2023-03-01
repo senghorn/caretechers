d3c406112884c27324e94849f5054392fcc4a833
benjhatch <benjamin.hat4@gmail.com>
2023-02-28 21:28:24 -0700

    changed back to aws_ip

--- Frontend/constants/config.js
+++ Frontend/constants/config.js
@@ -1,7 +1,7 @@
 const local_ip = '192.168.86.238';
 const aws_ip = 'ec2-54-241-210-48.us-west-1.compute.amazonaws.com';
 
-const ip = local_ip;
+const ip = aws_ip;
 
 export default {
   backend_server: 'http://' + ip + ':3000',
