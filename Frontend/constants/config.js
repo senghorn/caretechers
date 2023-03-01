e32b4a237d992feaeb18fd3379e7dd854c418091
Aaron Heo <aheo@qualtrics.com>
2023-02-28 18:44:59 -0700

    revert constants

--- Frontend/constants/config.js
+++ Frontend/constants/config.js
@@ -1,9 +1,4 @@
-const local_ip = '10.18.59.121';
-const aws_ip = 'ec2-54-241-210-48.us-west-1.compute.amazonaws.com';
-
-const ip = aws_ip;
-
 export default {
-  backend_server: 'http://192.168.0.150:3000', // Update config when testing.
+  backend_server: 'http://192.168.0.58:3000', // Update config when testing.
   backend_messaging: 'http://192.168.0.58:3001',
 };
