4ab4b290fd8fb8aa5151bb37440d30ccc551bd09
Aaron Heo <u1138155@utah.edu>
2023-03-03 03:34:55 +0000

    update backend server ip

--- Frontend/constants/config.js
+++ Frontend/constants/config.js
@@ -1,5 +1,5 @@
 const local_ip = '10.18.59.121';
-const aws_ip = 'ec2-54-241-210-48.us-west-1.compute.amazonaws.com';
+const aws_ip = 'ec2-13-56-226-47.us-west-1.compute.amazonaws.com';
 
 const ip = aws_ip;
 
