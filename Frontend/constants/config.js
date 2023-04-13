b8e2c1ff8d8f3066c688ed4bd21b6ad6651e072b
Aaron Heo <aheo@qualtrics.com>
2023-04-13 17:56:56 -0600

    revert constants

--- Frontend/constants/config.js
+++ Frontend/constants/config.js
@@ -1,6 +1,6 @@
-const local_ip = 'localhost';
+const local_ip = '10.18.86.123';
 const aws_ip = 'ec2-54-177-61-20.us-west-1.compute.amazonaws.com';
-const ip = local_ip;
+const ip = aws_ip;
 
 export default {
   ip,
