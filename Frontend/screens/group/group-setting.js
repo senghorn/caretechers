ed9fa4a32ec1a6b078200ed0c394ca7185411059
senghorn <rithsenghorn@gmail.com>
2023-04-23 18:19:51 -0600

    remove change name button

--- Frontend/screens/group/group-setting.js
+++ Frontend/screens/group/group-setting.js
@@ -222,9 +222,6 @@ export default function GroupSettings({ navigation }) {
           >
             <View style={styles.titleBox}>
               <Text style={styles.titleText}>{group.name}</Text>
-              <TouchableOpacity onPress={changeGroupNameHandler}>
-                <Text style={styles.changeName}>Change name</Text>
-              </TouchableOpacity>
             </View>
           </ImageBackground>
           <View style={styles.buttonList}>
@@ -576,11 +573,6 @@ const styles = StyleSheet.create({
     padding: 5,
     fontSize: 16,
     marginLeft: 10,
-  },
-  changeName: {
-    fontSize: 12,
-    color: colors.white,
-    marginTop: 15,
   },
   title: {
     fontSize: 18,
